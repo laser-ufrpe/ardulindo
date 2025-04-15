b18357c8aaea11bc59f6009cdb2e1a8facdf8bb9
Emmanuel N. C. Brito <code@yet1dev.com>
2025-04-15 17:41:03 -0300

    [rule] ardulindo >> Add enviroment variables to control pins

--- boot.py
+++ boot.py
@@ -6,3 +6,16 @@ from machine import Pin, ADC, PWM
 
 import ble_repl
 ble_repl.start()
+#===========================================================
+#                  ENVIROMENT VARIABLES
+#===========================================================
+isBlack = 1           # Tape color
+maxSpeed = 700        # Max PWM Speed     (0-1023)
+blackValue = 3500     # Set What is Black (0-4095) 
+
+Motors = [2, 4, 0, 15]                # set motors pins
+Sensor = [27, 26, 25, 33, 32, 35, 34] # sensor pins
+
+Stop = [0,0,0,0]                     # stop direction
+Front, Back = [0,1,0,1], [1,0,1,0]   # front and back directions
+Right, Left = [1,0,0,0], [0,0,1,0]   # right and left directions
