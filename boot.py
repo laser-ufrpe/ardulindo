5ceffacd1564f6b0e616f19c58a6080e38dec4ba
Emmanuel N. C. Brito <code@yet1dev.com>
2025-04-15 17:39:44 -0300

    [conf] ardulindo >> Add code for allow programming with bluetooth

---
+++ boot.py
@@ -0,0 +1,8 @@
+#===========================================================
+#                      BLUETOOTH CODE
+#===========================================================
+import time
+from machine import Pin, ADC, PWM
+
+import ble_repl
+ble_repl.start()
